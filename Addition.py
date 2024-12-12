5103b457e213f90a16a8b714aa17d947b050a02f
senda1988 <zidi.senda@gmail.com>
2024-12-12 13:49:23 +0100

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,9 @@
+#abfragen Zahl 1 und Zahl 2
+
+zahl1 = input("Zahl 1 bitte: ")
+zahl2 = input("Zahl 2 bitte: ")
+
+# Summer
+
+s = int(zahl1) + int(zahl2)
+print(f"Die Summe ist: {s}")
--- Namen.py
+++ Namen.py
@@ -1,5 +1,8 @@
 # Fragt Den Vor und Nachnamen über input ab
 
-vor-name = input("Dein Vorname Bitte: ")
-nach-name = input("Dein nachname bitte: ")
+vor_name = input("Dein Vorname bitte: ")
+nach_name = input("Dein nachname bitte: ")
 
+# Nutzt print(f, um Euren Vornamen und Nachnamen auf der Konsole auszugeben
+
+print(f"Dein Vorname ist {vor_name} und dein Nachname ist {nach_name}")
