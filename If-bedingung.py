89c1ccf177ef7bba0f019bca80fd061bd429f106
senda1988 <zidi.senda@gmail.com>
2024-12-12 14:02:21 +0100

    Update If-bedingung.py

--- If-bedingung.py
+++ If-bedingung.py
@@ -1,9 +1,9 @@
 
 zahl = int(input("Eingabe: "))
 
-if zahl<0 :
- print("Die Zahl ist negativ")
-elif zahl == 0 :
+if zahl == 0 :
  print("Die Zahl ist Null")
+elif zahl<0 :
+ print("Die Zahl ist negativ")
 else:
  print("Die Zahl ist positiv")
